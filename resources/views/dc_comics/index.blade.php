d4d2e58a440519bd1c43005a9aec7995b65620ac
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-29 16:49:49 +0200

    Fix : bootstrap classes

--- resources/views/dc_comics/index.blade.php
+++ resources/views/dc_comics/index.blade.php
@@ -5,25 +5,25 @@
 @section('content')
 
 <div class="container">
-    <div class="d-flex justify-content-between align-items-center">
+    <div class="d-flex justify-content-between align-items-center my-5">
         <h1>Fumetti DC</h1>
         <a href="{{ route('dc_comics.create') }}" class="btn btn-secondary">Nuovo Fumetto</a>
     </div>    
 </div>
 <div class="container">
-    <div class="row row-cols-4">
+    <div class="row row-cols-4 row-gap-5">
         @foreach ($comics as $comic)
             <div class="col">
                 <div class="card">
-                    <img src="{{ $comic->thumb }}" height="350" width="250" alt="" class="card-img-top">
+                    <img src="{{ $comic->thumb }}" height="360" width="240" alt="" class="card-img-top">
                     <div class="card-body">
                         <a href="{{ route('dc_comics.show', $comic) }}">
-                            <h5 class="card-title">Titolo: {{ $comic->title }}</h5>
+                            <h5 class="card-title pb-3">Titolo: {{ $comic->title }}</h5>
                         </a>
-                        <h5 class="card-subtitle">Serie: {{ $comic->series }}</h5>
+                        <h5 class="card-subtitle pb-3">Serie: {{ $comic->series }}</h5>
                         <p class="card-text">
                             <span>Prezzo: {{ $comic->price }}</span>
-                            <span>Venduto il: {{ $comic->sale_date }}</span>
+                            <span> Venduto il: {{ $comic->sale_date }}</span>
                         </p>
                     </div>
                 </div>
--- resources/views/dc_comics/show.blade.php
+++ resources/views/dc_comics/show.blade.php
@@ -4,11 +4,17 @@
 
 @section('content')
 
-<div class="container">
-    <img src="{{ $comic->thumb }}" alt="">
-    <h2>{{ $comic->title }}</h2>
-    <h4>{{ $comic->series }}</h4>
-    <p>{{ $comic->description }}</p>
+<div class="container my-5">
+    <div class="row">
+        <div class="col">
+            <img src="{{ $comic->thumb }}" alt="" class="d-inline-block">
+        </div>        
+        <div class="col">
+            <h2 class="pb-5">{{ $comic->title }}</h2>
+            <h4 class="pb-5">{{ $comic->series }}</h4>
+            <p>{{ $comic->description }}</p>
+        </div>
+    </div>    
 </div>
     
 @endsection
